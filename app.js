081854cb800e13f7072ea306bc99e0bd14b9a962
Toshiki Kato <78700184+tkato1@users.noreply.github.com>
2022-09-11 17:20:14 -0400

    add photos and on click info

--- app.js
+++ app.js
@@ -26,15 +26,23 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
+  const result = fruitsArray.filter(item => checkSugar(item));
+  console.log(result);
 
+function checkSugar(item) {
+  return item.nutritions.sugar > 15;
+}
   // TODO: Create a new HTML element to display your data
-
+  let text = "Fruits with sugar levels higher than 15 are"
+  for (let i = 0; i < result.length; i++) {
+   text += " " + result[i].name + ", ";
+  }
   // TODO: Append your new element to the page
-
+  gallery.append(text)
 }
 
 // SAMPLE CODE of how to create and append a new HTML element to the page
